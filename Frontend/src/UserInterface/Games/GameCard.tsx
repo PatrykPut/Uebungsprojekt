572399abcb1c5a82d689cf270c87eb95a263b5d8
PatrykPut <patryk.put@lise.de>
2023-10-12 14:48:19 +0200

    Drehende Karten

--- Frontend/src/UserInterface/Games/GameCard.tsx
+++ Frontend/src/UserInterface/Games/GameCard.tsx
@@ -1,11 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-const GameContainer = styled.div`
+const CardContainer = styled.div`
+    perspective: 1500px;
+    transform-style: preserve-3d;
     width: 250px;
-    height: 300px;
-    justify-content: center;
+    height: 320px;
+    position: relative;
+    transition: transform 0.6s;
     margin-bottom: 20px;
+
+    &:hover {
+      transform: rotateY(180deg);
+    }
+`;
+
+const GameContainer = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
     padding: 20px;
     border: white solid 1px ;
     border-radius: 10px;
@@ -13,10 +27,12 @@ const GameContainer = styled.div`
     box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
     margin-right: 10px;
     cursor: pointer;
+    backface-visibility: hidden;
+    overflow: auto;
+`;
 
-    &:hover {
-      border: black solid 1px;
-    }
+const GameContainerBack = styled(GameContainer)`
+    transform: rotateY(180deg);
 `;
 
 const Image = styled.img`
@@ -49,17 +65,16 @@ export function GameCard({ game }: {game : Game}) {
     }
   
     return (   
-     <GameContainer onClick={() => {
-      handleClick();
-     }}
-     onMouseOver={() => {
-      showDescription();
-     }}
-     >   
-          <Image src={process.env.PUBLIC_URL + game.image} alt={game.name} />                    
-          <h3>{game.name}</h3>    
-          <p>{game.releaseDate}</p>    
-          <p>{game.developer}</p>  
-     </GameContainer>
-      );    
+      <CardContainer>
+        <GameContainer onClick={handleClick}>   
+            <Image src={process.env.PUBLIC_URL + game.image} alt={game.name} />                    
+              <h3>{game.name}</h3>    
+              <p>{game.releaseDate}</p>    
+              <p>{game.developer}</p>  
+        </GameContainer>
+        <GameContainerBack onClick={handleClick}>
+          <p>{game.description}</p>
+        </GameContainerBack>
+     </CardContainer>
+    );
   }    
